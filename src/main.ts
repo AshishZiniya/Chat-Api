5cef119d4471c6b0b5a1dacab95808084c362722
wdcs-ashishziniya <ashish.ziniya@webcluesinfotech.com>
2025-10-29 12:54:00 +0530

    Created Chat App

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
-import cors from 'cors';
 
 const port = process.env.PORT || 10000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  const origins = ['http://localhost:3000'];
+  const origins = [
+    'http://localhost:3000',
+    'https://chat-app-beta-two-12.vercel.app',
+  ];
   if (process.env.FRONTEND_URL) {
     origins.push(process.env.FRONTEND_URL);
   }
-  app.use(
-    cors({
-      origin: [...origins],
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
+  app.enableCors({
+    origin: [...origins],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(port, () => {
     console.log(`NestJS chat server listening on http://localhost:${port}`);
   });
