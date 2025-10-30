0ec66b59bf47e447e25fed8467e79c501daf23cd
wdcs-ashishziniya <ashish.ziniya@webcluesinfotech.com>
2025-10-30 18:33:04 +0530

    Fixed

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Message, MessageSchema } from './messages.schema';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { EncryptionModule } from '../encryption/encryption.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Message.name, schema: MessageSchema }]),
+    EncryptionModule,
   ],
   controllers: [MessagesController],
   providers: [MessagesService],
